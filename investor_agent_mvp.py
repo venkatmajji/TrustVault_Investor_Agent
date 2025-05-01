4422a8fe77ec1b68a6e06519e1df7f244a1d370a
venkatmajji <52803952+venkatmajji@users.noreply.github.com>
2025-05-01 11:01:29 -0400

    Update investor_agent_mvp.py

--- investor_agent_mvp.py
+++ investor_agent_mvp.py
@@ -1,5 +1,6 @@
 
-from langchain.chat_models import ChatOpenAI
+
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
